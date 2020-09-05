3b42cb49932180ff5844ea85c8d8fa109b79a46a
Xinyao Niu <morryniu123@gmail.com>
2020-09-05 15:26:53 +1000

    fix submission

--- submit/xinyaon-seqalignomp.cpp
+++ submit/xinyaon-seqalignomp.cpp
@@ -128,7 +128,7 @@ int getMinimumPenalty(std::string x, std::string y, int pxy, int pgap,
 
     const int ROW = m+1;
     const int COL = n+1;
-    const int N_CORE = 6;
+    const int N_CORE = omp_get_max_threads();
     
     omp_set_num_threads(N_CORE);
 
