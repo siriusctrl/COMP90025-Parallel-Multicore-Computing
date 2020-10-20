ca08007fe4a1c21d48780b4b94f431a16ee53f90
Xinyao Niu <morryniu123@gmail.com>
2020-10-21 00:18:23 +1100

    now can run but has less solution

--- nqueue/naive_parallel.cpp
+++ nqueue/naive_parallel.cpp
@@ -8,10 +8,11 @@ using std::cin;
 using std::cout;
 using std::endl;
 
-void find_solution(int n, int max_solution);
+void find_solution(int n, int max_solution, int rank);
 bool check_acceptable(const vector<int> &queen_rows);
 
 const MPI_Comm comm = MPI_COMM_WORLD;
+constexpr int TERMINATION_SIGN = -1;
 
 int main(int argc, char *argv[])
 {
@@ -25,39 +26,110 @@ int main(int argc, char *argv[])
     MPI_Init_thread(&argc, &argv, MPI_THREAD_MULTIPLE, &prov);
     MPI_Comm_rank(comm, &rank);
 
-    cout << rank << endl;
+    int size {0};
+    MPI_Comm_size(comm, &size);
 
-    find_solution(n, max_solutions);
+    if (rank == 0)
+    {
+        MPI_Status status;
+        int finished {0};
+        int n_solutions {0};
+
+        while (true)
+        {
+            if (finished == size-1) {
+                cout << "found " << n_solutions << " solutions!" << endl;
+                break;
+            }
+
+            // cout << "finished " << finished << endl;
+
+            int res[n];
+
+            MPI_Recv(res, n, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, comm, &status);
+
+            if (res[0] == TERMINATION_SIGN) {
+                finished++;
+            } else {
+                n_solutions++;
+
+                if (n_solutions <= max_solutions) {
+                    for (int i=0; i < n; i++)
+                    {
+                        cout << res[i] << " ";
+                    }
+
+                    cout << endl;
+                }
+            }
+
+            if (n_solutions > max_solutions) 
+            {
+                bool temp {false};
+                MPI_Send(&temp, 1, MPI_CXX_BOOL, status.MPI_SOURCE, 0, comm);
+                finished++;
+            } else {
+                bool temp {true};
+                MPI_Send(&temp, 1, MPI_CXX_BOOL, status.MPI_SOURCE, 0, comm);
+            }
+
+        }
+
+    } else {
+        find_solution(n, max_solutions, rank);
+    }
+    
     MPI::Finalize();
 
     return 0;
 }
 
 
-void find_solution(int n, int max_solution) 
+void find_solution(int n, int max_solution, int rank) 
 {
     int solution[n] = {0};
     int n_solutions {0};
     vector<int> permutation{};
 
+    int counter {1};
+
     for (int i=0; i<n; ++i) {
         permutation.push_back(i);
     }
 
+    int size;
+    MPI_Comm_size(comm, &size);
+
     do {
-        if (check_acceptable(permutation)) {
-            n_solutions++;
 
-            for (auto p: permutation) {
-                cout << p << " ";
+        if (counter % size == rank)
+        {
+
+            if (check_acceptable(permutation)) 
+            {
+                MPI_Send(&permutation.front(), n, MPI_INT, 0, 0, comm);
+
+                MPI_Status status;
+                bool feedback;
+                MPI_Recv(&feedback, 1, MPI_CXX_BOOL, 0, MPI_ANY_TAG, comm, &status);
+
+                if (!feedback) 
+                {
+                    // cout << "node " << rank << " stopped" << endl;
+                    return;
+                }
             }
-
-            cout << endl;
         }
+
+        counter++;
     } 
     while(n_solutions < max_solution && std::next_permutation(permutation.begin(), permutation.end()));
 
-    cout << max_solution << " solutions found!" << endl;
+    cout << "node " << rank << " has no more solution to try" << endl;
+
+    // no more candidates to try
+    permutation[0] = TERMINATION_SIGN;
+    MPI_Send(&permutation.front(), n, MPI_INT, 0, 0, comm);
 }
 
 
@@ -77,3 +149,4 @@ bool check_acceptable(const vector<int> &queen_rows)
 	return true;
 }
 // mpicxx -std=c++14 naive_parallel.cpp -o naive_parallel -O3
+// mpirun -np 4 naive_parallel 8 1
